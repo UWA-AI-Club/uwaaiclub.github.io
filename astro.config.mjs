b04ee6d67055d831abe1ace332c87b893fd93782
Jason <66883665+json20@users.noreply.github.com>
2025-11-17 18:50:51 +0800

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://uwa-ai-club.github.io',
+  base: '/website',
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
